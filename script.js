01961393515fd6ca251bd88ba424bc01ab34dee3
Saurabh Dombe <saurabhdombe83@gmail.com>
2025-09-26 11:45:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
+	event.preventDefault();
+  let firstName = document.querySelector('input[name=fname]').vlaue.trim();
+  let lastName = document.querySelector('input[name=lname]').value.trim();
 
+  let fullName = (firstName + "" + lastName);
+	alert(fullName);
 }
